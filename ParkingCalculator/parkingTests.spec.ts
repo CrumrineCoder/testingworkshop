1fc37207781042e2283276aed39d3acb072cb41c
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-07 12:42:40 -0500

    dedicated nav function

--- ParkingCalculator/parkingTests.spec.ts
+++ ParkingCalculator/parkingTests.spec.ts
@@ -1,24 +1,28 @@
 import { test, expect } from "playwright/test";
 
-test("tab title test", async ({ page }) => {
+async function navHomePage(page: any) {
   await page.goto("https://www.shino.de/parkcalc/index.php");
+}
+
+test("tab title test", async ({ page }) => {
+  await navHomePage(page);
   const title = await page.title();
   expect(title).toBe("Parking Cost Calculator");
 });
 
 test("dropdown options test", async ({ page }) => {
-    await page.goto("https://www.shino.de/parkcalc/index.php");
-  
-    const dropdown = await page.locator("select[id='ParkingLot']");
-    await expect(dropdown).toBeVisible();
-  
-    const options = await dropdown.locator("option").allTextContents();
-  
-    expect(options).toEqual([
-      "Valet Parking",
-      "Short-Term Parking",
-      "Economy Parking",
-      "Long-Term Garage Parking",
-      "Long-Term Surface Parking",
-    ]);
-  });
+  await navHomePage(page);
+
+  const dropdown = await page.locator("select[id='ParkingLot']");
+  await expect(dropdown).toBeVisible();
+
+  const options = await dropdown.locator("option").allTextContents();
+
+  expect(options).toEqual([
+    "Valet Parking",
+    "Short-Term Parking",
+    "Economy Parking",
+    "Long-Term Garage Parking",
+    "Long-Term Surface Parking",
+  ]);
+});
