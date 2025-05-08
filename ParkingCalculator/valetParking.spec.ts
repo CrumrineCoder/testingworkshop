893d8ded84a4e45a1d8e330c94fc61d4a27a42f0
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-08 14:21:09 -0500

    Validate time & cost

--- ParkingCalculator/valetParking.spec.ts
+++ ParkingCalculator/valetParking.spec.ts
@@ -1,15 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-// Test 1 day interval ($18)
-test('Default Valet Parking Midnight 1 day Interval', async ({ page }) => {
-  await page.goto('https://www.shino.de/parkcalc/index.php');
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByRole('row', { name: 'Please input entry date and' }).getByRole('link').click();
+// Test 1 day interval for Valet Parking. This should be $18 since it's 1 day difference.
+test("Default Valet Parking Midnight 1 day Interval", async ({ page }) => {
+  await page.goto("https://www.shino.de/parkcalc/index.php");
+  const page1Promise = page.waitForEvent("popup");
+  await page
+    .getByRole("row", { name: "Please input entry date and" })
+    .getByRole("link")
+    .click();
   const page1 = await page1Promise;
-  await page1.getByRole('link', { name: '8', exact: true }).click();
-  const page2Promise = page.waitForEvent('popup');
-  await page.getByRole('cell', { name: 'MM/DD/YYYY Pick a date 12:00' }).getByRole('link').click();
+  await page1.getByRole("link", { name: "8", exact: true }).click();
+  const page2Promise = page.waitForEvent("popup");
+  await page
+    .getByRole("cell", { name: "MM/DD/YYYY Pick a date 12:00" })
+    .getByRole("link")
+    .click();
   const page2 = await page2Promise;
-  await page2.getByRole('link', { name: '9', exact: true }).click();
-  await page.getByRole('button', { name: 'Calculate' }).click();
+  await page2.getByRole("link", { name: "9", exact: true }).click();
+  await page.getByRole("button", { name: "Calculate" }).click();
+
+  const FinalCost = await page.locator("span[class='SubHead']").nth(0).textContent();
+  expect(FinalCost?.trim()).toBe("$ 18.00"); // Use trim() to remove any extra whitespace
+
+  const FinalTime = await page.locator("span[class='BodyCopy']").textContent();
+  expect (FinalTime?.trim()).toBe("(1 Days, 0 Hours, 0 Minutes)")
 });
