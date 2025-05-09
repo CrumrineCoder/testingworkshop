35a2c9bf56ccfbd5e1bf7fbe9707e197c5b83529
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-09 15:54:52 -0500

    Expected value

--- ParkingCalculator/sameDayTests.spec.ts
+++ ParkingCalculator/sameDayTests.spec.ts
@@ -1,147 +1,146 @@
 import {
-  PressCalculate,
-  ValidateParkingCost,
-  ValidateTime,
-  inputCalendarDate,
-  inputDay,
-  inputTime,
+    PressCalculate,
+    ValidateParkingCost,
+    ValidateTime,
+    inputCalendarDate,
+    inputDay,
+    inputTime,
 } from "./FormFunctions.spec";
 
 // Test 1 day interval for Valet Parking using calendars
 export async function testMidnight1DayIntervalWithCalendars({
-  page,
+    page,
 }: {
-  page: any;
-}) {
-  await inputCalendarDate({ page }, "entry", 8);
-  await inputCalendarDate({ page }, "leaving", 9);
+    page: any;
+}, expectedValue: string) {
+    await inputCalendarDate({ page }, "entry", 8);
+    await inputCalendarDate({ page }, "leaving", 9);
 
-  await PressCalculate({ page });
+    await PressCalculate({ page });
 
-  await ValidateParkingCost({ page }, "$ 18.00");
-  await ValidateTime({ page }, "(1 Days, 0 Hours, 0 Minutes)");
+    await ValidateParkingCost({ page }, expectedValue);
+    await ValidateTime({ page }, "(1 Days, 0 Hours, 0 Minutes)");
 }
 
 // Test 1 day interval for Valet Parking using inputs
 export async function testMidnight1DayIntervalWithInputs({
-  page,
+    page,
 }: {
-  page: any;
-}) {
-  await inputDay({ page }, "StartingDate", "5/8/2025");
-  await inputDay({ page }, "LeavingDate", "5/9/2025");
+    page: any;
+}, expectedValue: string) {
+    await inputDay({ page }, "StartingDate", "5/8/2025");
+    await inputDay({ page }, "LeavingDate", "5/9/2025");
 
-  await PressCalculate({ page });
+    await PressCalculate({ page });
 
-  await ValidateParkingCost({ page }, "$ 18.00");
-  await ValidateTime({ page }, "(1 Days, 0 Hours, 0 Minutes)");
+    await ValidateParkingCost({ page }, expectedValue);
+    await ValidateTime({ page }, "(1 Days, 0 Hours, 0 Minutes)");
 }
 
 // Test same day 12-hour interval
 export async function testSameDay12HoursIntervalWithInputs({
-  page,
+    page,
 }: {
-  page: any;
-}) {
-  await inputDay({ page }, "StartingDate", "5/9/2025");
-  await inputDay({ page }, "LeavingDate", "5/9/2025");
+    page: any;
+}, expectedValue: string) {
+    await inputDay({ page }, "StartingDate", "5/9/2025");
+    await inputDay({ page }, "LeavingDate", "5/9/2025");
 
-  await inputTime({ page }, "StartingTime", "1:00");
-  await inputTime({ page }, "LeavingTime", "13:00");
+    await inputTime({ page }, "StartingTime", "1:00");
+    await inputTime({ page }, "LeavingTime", "13:00");
 
-  await PressCalculate({ page });
+    await PressCalculate({ page });
 
-  await ValidateParkingCost({ page }, "$ 18.00");
-  await ValidateTime({ page }, "(0 Days, 12 Hours, 0 Minutes)");
+    await ValidateParkingCost({ page }, expectedValue);
+    await ValidateTime({ page }, "(0 Days, 12 Hours, 0 Minutes)");
 }
 
 // Test same day 5-hour interval
 export async function testSameDay5HoursIntervalWithInputs({
-  page,
+    page,
 }: {
-  page: any;
-}) {
-  await inputDay({ page }, "StartingDate", "5/9/2025");
-  await inputDay({ page }, "LeavingDate", "5/9/2025");
+    page: any;
+}, expectedValue: string) {
+    await inputDay({ page }, "StartingDate", "5/9/2025");
+    await inputDay({ page }, "LeavingDate", "5/9/2025");
 
-  await inputTime({ page }, "StartingTime", "1:00");
-  await inputTime({ page }, "LeavingTime", "6:00");
+    await inputTime({ page }, "StartingTime", "1:00");
+    await inputTime({ page }, "LeavingTime", "6:00");
 
-  await PressCalculate({ page });
+    await PressCalculate({ page });
 
-  await ValidateParkingCost({ page }, "$ 12.00");
-  await ValidateTime({ page }, "(0 Days, 5 Hours, 0 Minutes)");
+    await ValidateParkingCost({ page }, expectedValue);
+    await ValidateTime({ page }, "(0 Days, 5 Hours, 0 Minutes)");
 }
 
 // Test same day 4-hour interval
 export async function testSameDay4HoursIntervalWithInputs({
-  page,
+    page,
 }: {
-  page: any;
-}) {
-  await inputDay({ page }, "StartingDate", "5/9/2025");
-  await inputDay({ page }, "LeavingDate", "5/9/2025");
+    page: any;
+}, expectedValue: string) {
+    await inputDay({ page }, "StartingDate", "5/9/2025");
+    await inputDay({ page }, "LeavingDate", "5/9/2025");
 
-  await inputTime({ page }, "StartingTime", "1:00");
-  await inputTime({ page }, "LeavingTime", "5:00");
+    await inputTime({ page }, "StartingTime", "1:00");
+    await inputTime({ page }, "LeavingTime", "5:00");
 
-  await PressCalculate({ page });
+    await PressCalculate({ page });
 
-  await ValidateParkingCost({ page }, "$ 12.00");
-  await ValidateTime({ page }, "(0 Days, 4 Hours, 0 Minutes)");
+    await ValidateParkingCost({ page }, expectedValue);
+    await ValidateTime({ page }, "(0 Days, 4 Hours, 0 Minutes)");
 }
 
 // Test same day 1-minute interval
 export async function testSameDay1MinuteIntervalWithInputs({
-  page,
+    page,
 }: {
-  page: any;
-}) {
-  await inputDay({ page }, "StartingDate", "5/9/2025");
-  await inputDay({ page }, "LeavingDate", "5/9/2025");
+    page: any;
+}, expectedValue: string) {
+    await inputDay({ page }, "StartingDate", "5/9/2025");
+    await inputDay({ page }, "LeavingDate", "5/9/2025");
 
-  await inputTime({ page }, "StartingTime", "1:00");
-  await inputTime({ page }, "LeavingTime", "1:01");
+    await inputTime({ page }, "StartingTime", "1:00");
+    await inputTime({ page }, "LeavingTime", "1:01");
 
-  await PressCalculate({ page });
+    await PressCalculate({ page });
 
-  await ValidateParkingCost({ page }, "$ 12.00");
-  await ValidateTime({ page }, "(0 Days, 0 Hours, 1 Minutes)");
+    await ValidateParkingCost({ page }, expectedValue);
+    await ValidateTime({ page }, "(0 Days, 0 Hours, 1 Minutes)");
 }
 
 // Test same day 5 hours 1 minute interval
 export async function testSameDay5Hours1MinuteIntervalWithInputs({
-  page,
+    page,
 }: {
-  page: any;
-}) {
-  await inputDay({ page }, "StartingDate", "5/9/2025");
-  await inputDay({ page }, "LeavingDate", "5/9/2025");
+    page: any;
+}, expectedValue: string) {
+    await inputDay({ page }, "StartingDate", "5/9/2025");
+    await inputDay({ page }, "LeavingDate", "5/9/2025");
 
-  await inputTime({ page }, "StartingTime", "1:00");
-  await inputTime({ page }, "LeavingTime", "6:01");
+    await inputTime({ page }, "StartingTime", "1:00");
+    await inputTime({ page }, "LeavingTime", "6:01");
 
-  await PressCalculate({ page });
+    await PressCalculate({ page });
 
-  await ValidateParkingCost({ page }, "$ 18.00");
-  await ValidateTime({ page }, "(0 Days, 5 Hours, 1 Minutes)");
+    await ValidateParkingCost({ page }, expectedValue);
+    await ValidateTime({ page }, "(0 Days, 5 Hours, 1 Minutes)");
 }
 
 // Test 1 day 1 minute interval
 export async function test1Day1MinuteIntervalWithInputs({
-  page,
+    page,
 }: {
-  page: any;
-}) {
-  await inputDay({ page }, "StartingDate", "5/8/2025");
-  await inputDay({ page }, "LeavingDate", "5/9/2025");
+    page: any;
+}, expectedValue: string) {
+    await inputDay({ page }, "StartingDate", "5/8/2025");
+    await inputDay({ page }, "LeavingDate", "5/9/2025");
 
-  await inputTime({ page }, "StartingTime", "1:00");
-  await inputTime({ page }, "LeavingTime", "1:01");
+    await inputTime({ page }, "StartingTime", "1:00");
+    await inputTime({ page }, "LeavingTime", "1:01");
 
-  await PressCalculate({ page });
+    await PressCalculate({ page });
 
-  // This is actually probably a bug, but it depends how the app is defining a day.
-  await ValidateParkingCost({ page }, "$ 36.00");
-  await ValidateTime({ page }, "(1 Days, 0 Hours, 1 Minutes)");
+    await ValidateParkingCost({ page }, expectedValue);
+    await ValidateTime({ page }, "(1 Days, 0 Hours, 1 Minutes)");
 }
--- ParkingCalculator/valetParking.spec.ts
+++ ParkingCalculator/valetParking.spec.ts
@@ -17,41 +17,41 @@ import {
 test("Valet Parking - Midnight 1 Day Interval with Calendars", async ({
   page,
 }) => {
-  await testMidnight1DayIntervalWithCalendars({ page });
+  await testMidnight1DayIntervalWithCalendars({ page }, "$ 18.00");
 });
 
 test("Valet Parking - Midnight 1 Day Interval with Inputs", async ({
   page,
 }) => {
-  await testMidnight1DayIntervalWithInputs({ page });
+  await testMidnight1DayIntervalWithInputs({ page }, "$ 18.00");
 });
 
 test("Valet Parking - Same Day 12 Hours Interval with Inputs", async ({
   page,
 }) => {
-  await testSameDay12HoursIntervalWithInputs({ page });
+  await testSameDay12HoursIntervalWithInputs({ page }, "$ 18.00");
 });
 
 test("Valet Parking - Same Day 5 Hours Interval with Inputs", async ({
   page,
 }) => {
-  await testSameDay5HoursIntervalWithInputs({ page });
+  await testSameDay5HoursIntervalWithInputs({ page }, "$ 12.00");
 });
 
 test("Valet Parking - Same Day 4 Hours Interval with Inputs", async ({
   page,
 }) => {
-  await testSameDay4HoursIntervalWithInputs({ page });
+  await testSameDay4HoursIntervalWithInputs({ page }, "$ 12.00");
 });
 
 test("Valet Parking - Same Day 1 Minute Interval with Inputs", async ({
   page,
 }) => {
-  await testSameDay1MinuteIntervalWithInputs({ page });
+  await testSameDay1MinuteIntervalWithInputs({ page }, "$ 12.00");
 });
 
 test("Valet Parking - 1 Day 1 Minute Interval with Inputs", async ({
   page,
 }) => {
-  await test1Day1MinuteIntervalWithInputs({ page });
+  await test1Day1MinuteIntervalWithInputs({ page }, "$ 36.00");
 });
