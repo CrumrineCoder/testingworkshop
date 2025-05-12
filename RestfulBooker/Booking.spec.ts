e56cf70f9ccc37a48207b8d28155dcc42c31c44a
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-12 14:21:54 -0500

    Get first booking

--- RestfulBooker/Booking.spec.ts
+++ RestfulBooker/Booking.spec.ts
@@ -1,7 +1,25 @@
 import { test, expect } from "playwright/test";
+
+// from https://www.geeksforgeeks.org/javascript-program-to-check-if-an-array-contains-only-unique-values/
+function checkDistinct(array) {
+    const checkSet = new Set(array);
+    return checkSet.size === array.length;  
+}
+
+let firstBookingid: number; 
+
 test("Check All Bookings", async ({request}) => {
-    const bookings = await request.get("https://restful-booker.herokuapp.com/booking")
+    const bookings = await request.get("https://restful-booker.herokuapp.com/booking");
     expect(bookings.ok()).toBeTruthy(); 
-    const bookingsJson = await bookings.json();
-    console.log(bookingsJson);
+    expect(bookings.status()).toBe(200);
+    const bookingsJson: { bookingid: number }[] = await bookings.json();
+    expect(checkDistinct(bookingsJson));
+    firstBookingid = bookingsJson[0]?.bookingid; 
 });
+
+test("Get First Booking", async ({request}) => {
+    const firstBooking = await request.get("https://restful-booker.herokuapp.com/booking/" + firstBookingid);
+    expect(firstBooking.ok()).toBeTruthy(); 
+    expect(firstBooking.status()).toBe(200);
+    console.log(await firstBooking.json());
+})
