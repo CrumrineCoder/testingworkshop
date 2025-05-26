bcf231c9b23ff16e250cedb5fb63619d337b4ded
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-25 19:26:49 -0500

    Update zod.ts

--- tests/zod/zod.ts
+++ tests/zod/zod.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-
+/*
 const userSchema = z.object({
   name: z.string(),
   age: z.number(),
@@ -16,3 +16,24 @@ try {
     console.log(err.issues);
   }
 }
+*/
+
+/*
+const settingsSchema = z.object({
+  theme: z.string().default("light"),
+  notifications: z.boolean().optional(),
+});
+
+const result = settingsSchema.parse({});
+console.log(result); // { theme: "light" }
+*/
+
+const passwordSchema = z.string().refine(val => val.length >= 8, {
+  message: "Password must be at least 8 characters long",
+});
+
+const passResult1 = passwordSchema.parse("124567");
+console.log(passResult1);
+
+const passResult2 = passwordSchema.parse("BatteryTifaInALandMine");
+console.log(passResult2);
